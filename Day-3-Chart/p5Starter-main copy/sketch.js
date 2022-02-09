abe3d85a616139c1de3a7834ef4a06cc49275f2f
Stephen-Gordon <N00202054@iadt.ie>
2022-02-09 11:27:51 +0000

    commit

--- Day-3-Chart/p5Starter-main copy/sketch.js
+++ Day-3-Chart/p5Starter-main copy/sketch.js
@@ -1,67 +1,85 @@
-
+let chartWidth = 400;
+let chartHeight = 400;
 
 
 //Bar height 
-let data = [50,70,100,200,40,50]
+let data = [50, 360, 100, 200, 40, 50]
 
-let margin = 20;
+let margin = 60;
 
 let spacing = 10
 
-let barWidth = 30
 
-let numTicks = 40;
 
-let tickDistance = 40;
+let numOfTicks = 10;
 
-//let numOfTicks = ;
+
+//Length of tickets
+let tickLength = 10;
+
+//space between ticks
+let tickSpacing = chartHeight / numOfTicks
+
+let ticketValues = chartHeight / numOfTicks
+
+let availableWidth = chartWidth - (margin * 2) - (spacing * (data.length));
+
+let barWidth = availableWidth / data.length;
+
+console.log(barWidth)
 
 
 function setup() {
     createCanvas(500, 500);
-    
+
 }
 
 function draw() {
     background(255)
 
-    fill(255,0,0)
-    textSize(50)
-    textAlign(RIGHT, CENTER)
-    text("0",200,200)
+    
 
+
+
+    
     // First translation
     translate(50, 450)
-    line(0,0,0,-400)
-    line(0,0,400,0)
-
-    // New translation
-    translate(margin,0)
-    stroke(0)
-    strokeWeight(2)
-    line(0,0,-10,0)
-    line(0,-40,-10,-40)
-    line(0,-80,-10,-80)
+    line(0, 0, 0, -chartHeight)
+    line(0, 0, chartWidth, 0)
 
 
 
-   // for(i = 0; i < ;)
+    //Ticks + SIde numbers loop
+    
 
-
-    for(i = 0; i <= numTicks; i++){
+    for (i = 0; i <= numOfTicks; i++) {
         stroke(0)
         strokeWeight(2)
-        line(0,-numTicks*-i,-10,-numTicks*-i)
-    
+        //Draws ticks
+        line(0, -i * tickSpacing, -tickLength, -i * tickSpacing)
+        
+        textSize(12)
+        textAlign(RIGHT, CENTER)
+        text(i * ticketValues,-15, -i * ticketValues)
 
     }
 
 
-     for(i = 0; i < data.length; i++){
-        rect((barWidth +spacing) * i, 0, barWidth, -data[i])
-    } 
+
+
+    // New translation
+    translate(margin, 0)
+
+    //Bar loop
+    for (i = 0; i < data.length; i++) {
+        fill(255, 0, 0)
+        rect((barWidth + spacing) * i, 0, barWidth, -data[i])
+        
+        text("Hello", (barWidth + spacing) * i, 0)
+
+    }
+
     
+
+
 }
-
-
-
