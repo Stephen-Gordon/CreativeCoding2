c0e6a713be1c5f63a06e68afc8fe5d2804e4b48f
Ste <n00202054@iadt.ie>
2022-03-08 18:35:20 +0000

    commit

--- Day-1-Functions/p5Starter-main/script.js
+++ Day-1-Functions/p5Starter-main/script.js
@@ -7,3 +7,31 @@ function clap(startNum,endNum){
     }
     
 }
+
+let myCars = [
+    {name:'Fiat', value:220, cc:1020, color:'red'},
+    {name:'VW', value:120,cc:1040,color:'blue'},
+    {name:'BMW', value:190,cc:900,color:'green'}
+ ];
+
+function setup(){
+    
+ createCanvas(400,400)
+ 
+
+}
+
+function draw(){
+    push();
+    translate(width / 2, -height / 2)
+    fill(255,0,0)
+    rotate(PI/4)
+    rectMode(CENTER)
+    rect(width / 2, height / 2,50,10)
+    pop();
+
+    /* for(i = 0; i<= myCars.length; i++){
+        //console.log(myCars.length)
+    } */
+    console.log(myCars.length)
+}
